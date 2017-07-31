8757a3f91eedf85c6fe0f33db02129d7c24e52a6
nirmala22 <nirmala@sofs.in>
2017-07-31 05:13:04 -0700

    donate files

---
+++ donate.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<?php
+$page_id="donate";
+?>
+<html class="no-js" lang="">
+    
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Anbu Home-Donation</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.theme.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+         <link rel="stylesheet" href="css/responsive.css">
+        <script src="js/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+       
+            
+         <header>
+          
+          <!-- header logo: style can be found in header.less -->
+       <?php include_once './includes/head.php'; ?>
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+            <!-- Left side column. contains the logo and sidebar -->
+      
+            
+     </header>
+        <div class="page-title events-title text-center">
+          <h2 class="text-extra-bold white text-uppercase">Donation</h2>
+          <div class="page-location direct text-center display-ib">
+            <span class="ubuntu fz-14 green-5c">Home<i class="gray-e9"> / </i>Donation</span>
+          </div>
+        </div>
+
+      <section class="mt-150 about-us-main mb-150">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-md-12 line1" ></div><br>
+
+                <div class="col-md-12">
+              <h4 class="d-black lh-33 h-sep">Donations</h4>
+              <p class="fz-15 gray-777 lh-28 mt-30">Anbu Illam runs purely on donations and sponsorship.</p>
+
+              <p class="lh-28 gray-777 fz-15 mt-15">We do not want to compromise on the necessities we have assured to the inmates. We do not want them to lose hope again on the mankind, as for many inmates, Anbu Illam is the only hope for existence. Hence we request the public, philanthropists, corporate and other institutions to come forward, join the endeavour, embrace our noble cause and help us build a better living atmosphere for the needy.</p> 
+              <p class="lh-28 gray-777 fz-15 mt-15">All the donations will be acknowledged with a receipt. Donations can be sent in cash/kind/DD/cheque in the name of Anbu Illam. All donations are exempted from income tax under 80G and 35 AC of Income Tax Act of 1961. We solicit your cooperation and support to transform old age home into a model institution.</p>
+              <p class="fz-15 gray-777 lh-28 mt-15">Presently we have 20 elders (Females 0 and Males 0) who lead a Blissful life as one big family and they call it their Home.</p>
+	          
+	           </div>    
+               </div>
+               </div>
+               </section>
+                     
+              
+
+       <footer>
+          <?php include_once './includes/nav.php'; ?>
+            <!-- Right side column. Contains the navbar and content of the page -->
+            
+        </footer>
+        <script src="../../../ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/jquery.mixitup.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+
+</html>
